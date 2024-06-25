1bf743e8681ce03077107777a43380ea1393e682
Tariq Safieh <safieh.tariq@gmail.com>
2024-06-25 15:16:06 +0000

    admin managment add

--- Lucke/diary/admin.py
+++ Lucke/diary/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
+from django.contrib.auth import get_user_model
 
 # Register your models here.
+
+CustomUser = get_user_model()
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at', 'author')
