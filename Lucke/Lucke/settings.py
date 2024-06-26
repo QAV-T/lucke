b7f9ec58890ca809c3018b3a7166bfb7eeab7421
Tariq Safieh <safieh.tariq@gmail.com>
2024-06-26 18:00:50 +0000

    Migrate to PostgreSQL

--- Lucke/Lucke/settings.py
+++ Lucke/Lucke/settings.py
@@ -86,12 +86,24 @@ WSGI_APPLICATION = 'Lucke.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cough_elm_grasp_626997',
+        'USER': 'uzg7xvjb2fb',
+        'PASSWORD': 'CaCUuFK5U2pM',
+        'HOST': 'ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
