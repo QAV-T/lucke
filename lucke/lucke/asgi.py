80cab84310abc847bb77ba9cc66d0deda0ca4f76
Tariq Safieh <safieh.tariq@gmail.com>
2024-06-27 22:39:50 +0000

    Fix sensitivity issues

--- lucke/lucke/asgi.py
+++ lucke/lucke/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Lucke project.
+ASGI config for lucke project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- lucke/lucke/wsgi.py
+++ lucke/lucke/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Lucke project.
+WSGI config for lucke project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
