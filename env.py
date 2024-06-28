49e34d2b70ae0d427a4c84ef515825b43f81027d
Tariq Safieh <safieh.tariq@gmail.com>
2024-06-28 08:15:57 +0000

    allwed host edit

---
+++ env.py
@@ -0,0 +1,6 @@
+
+import os
+
+os.environ['DJANGO_SECRET_KEY'] = '^6g*!ra5-u3ai^2wb9c!upsy64_=#3v&k65@+da$j5)dde)b^_'
+os.environ['DEBUG'] = 'True'
+os.environ['DATABASE_URL'] = 'postgres://uzg7xvjb2fb:CaCUuFK5U2pM@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/cough_elm_grasp_626997'
