343533cbb41d60f6c6ca73dbd8ad2808beb3fdbc
Tariq Safieh <safieh.tariq@gmail.com>
2024-07-01 20:38:25 +0000

    Update settings

--- lucke/lucke/settings.py
+++ lucke/lucke/settings.py
@@ -148,6 +148,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+print(STATICFILES_DIRS)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
