409f228db20c0d57bccb5c33f4da13488ef0f897
Tariq Safieh <safieh.tariq@gmail.com>
2024-07-02 07:07:26 +0000

    Adding Sidenote model to Admin

--- lucke/diary/admin.py
+++ lucke/diary/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from .models import Diary
+from .models import Diary, Sidenote
 from django.contrib.auth import get_user_model
 
 # Register your models here.
 
 CustomUser = get_user_model()
 
-@admin.register(Diary)
-class PostAdmin(admin.ModelAdmin):
+class SideNoteInline(admin.TabularInline):
+    model = Sidenote
+    extra = 1
+class DiaryAdmin(admin.ModelAdmin):
+    inlines = [SideNoteInline]
     list_display = ('title', 'author', 'created_at')
     search_fields = ('title', 'content')
     list_filter = ('created_at', 'author')
+
+admin.site.register(Diary, DiaryAdmin)
+admin.site.register(Sidenote)
